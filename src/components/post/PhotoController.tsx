430a1e48e3190716db542937459fef556d29b848
Pleiades S <kashiashin@gmail.com>
2023-09-23 14:01:49 +0900

    fix to thumb src

--- src/components/post/PhotoController.tsx
+++ src/components/post/PhotoController.tsx
@@ -91,7 +91,7 @@ function PhotoController() {
                   'h-20 w-20 border-2 border-solid object-cover',
                   selected ? 'border-slate-100' : 'border-slate-900',
                 )}
-                src={image.src}
+                src={image.thumbSrc}
               />
             </button>
           );
