13c5c2706f7fd8bb9282660584411356e43de4cf
Pleiades S <kashiashin@gmail.com>
2023-09-26 16:36:04 +0900

    save it to stroage

--- src/atoms/auth/atom.ts
+++ src/atoms/auth/atom.ts
@@ -1,6 +1,7 @@
 import { atom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
 
 import type { Song } from '@/types/server.type';
 
-export const selectedPickSong = atom<Song[]>([]);
+export const selectedPickSong = atomWithStorage<Song[]>('@auth.top3Songs', []);
 export const signInLoading = atom<boolean>(false);
