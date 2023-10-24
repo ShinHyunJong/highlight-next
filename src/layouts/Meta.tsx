58cd6ee6da10fdeff1516426fb930d9511bcdf0e
Pleiades S <kashiashin@gmail.com>
2023-10-24 14:49:20 +0900

    favicon fix2

--- src/layouts/Meta.tsx
+++ src/layouts/Meta.tsx
@@ -8,9 +8,24 @@ const Meta = () => {
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1"
       />
-      <link rel="apple-touch-icon" href="apple-touch-icon.png" key="apple" />
-      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   );
 };
