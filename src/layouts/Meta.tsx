2647a57837028aa3454654481b3b29e73e48462e
Pleiades S <kashiashin@gmail.com>
2023-10-24 14:43:38 +0900

    favicon fix

--- src/layouts/Meta.tsx
+++ src/layouts/Meta.tsx
@@ -9,7 +9,8 @@ const Meta = () => {
         content="width=device-width, initial-scale=1.0, maximum-scale=1"
       />
       <link rel="apple-touch-icon" href="apple-touch-icon.png" key="apple" />
-      <link rel="icon" href="favicon.ico" key="favicon" />
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
     </Head>
   );
 };
